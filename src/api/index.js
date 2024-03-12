65ca63e01feb6c97dbb03bc47fb1f3292f902bb9
lakshayystrideAhead <lakshay.saini@strideahead.in>
2024-03-12 23:29:37 +0530

    updated code with changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,29 +1,3 @@
-// import axios from 'axios';
-
-// export const getPlacesData = async (type, sw, ne) => {
-
-//     try {
-//         const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
-//             params: {
-//                 bl_latitude: sw.lat,
-//                 tr_latitude: ne.lat,
-//                 bl_longitude: sw.lng,
-//                 tr_longitude: ne.lng,
-//             },
-//             headers: {
-//                 'X-RapidAPI-Key': '821542c86dmshef3c50fb0235f2dp13506ejsn48bebaae9026',
-//                 'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-//             },
-//         });
-
-//         return data;
-
-//     } catch (error) {
-//     //   console.log(error);
-
-//     }
-// }
-
 import axios from 'axios';
 
 export const getPlacesData = async(type, sw, ne) => {
